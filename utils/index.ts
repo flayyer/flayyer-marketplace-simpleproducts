f5d6c6a252bb06ad52ac05463ed7b9c647a1d37a
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-09-15 20:13:45 -0300

    feat: Improve useFormatter

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import {useMemo} from 'react';
+
 export function isFiniteNumber(value: any): value is number {
   return Number.isFinite(value);
 }
@@ -6,13 +8,19 @@ export function useFormatter(
   locale: string | undefined,
   currency: string | undefined,
 ) {
-  try {
-    return new Intl.NumberFormat(locale, {
-      style: 'currency',
-      currency,
-      currencyDisplay: 'symbol', // 'narrowSymbol' fails on Safari iOS
-    });
-  } catch {
-    return null;
-  }
+  return useMemo(() => {
+    try {
+      return new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency,
+        currencyDisplay: 'narrowSymbol', // 'narrowSymbol' fails on Safari iOS
+      });
+    } catch {
+      return new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency,
+        currencyDisplay: 'symbol', // Fallback
+      });
+    }
+  }, [currency, locale]);
 }
